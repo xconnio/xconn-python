7bb19308345bae686d2773d6a716778838ce9f29
Omer Akram <omer@thing.com>
2024-04-26 20:16:17 +0500

    add wamp session implementation using wampproto.py

--- wamp/types.py
+++ wamp/types.py
@@ -1,5 +1,43 @@
+from asyncio import Future
+from dataclasses import dataclass
+from typing import Callable
+
 from simple_websocket import Client
-from wampproto import joiner, serializers
+from wampproto import messages, joiner, serializers
+
+
+@dataclass
+class Registration:
+    registration_id: int
+
+
+@dataclass
+class RegisterRequest:
+    future: Future[Registration]
+    endpoint: Callable[[messages.Invocation], messages.Yield]
+
+
+@dataclass
+class UnregisterRequest:
+    future: Future
+    registration_id: int
+
+
+@dataclass
+class Subscription:
+    subscription_id: int
+
+
+@dataclass
+class SubscribeRequest:
+    future: Future[Subscription]
+    endpoint: Callable[[messages.Event], None]
+
+
+@dataclass
+class UnsubscribeRequest:
+    future: Future[messages.UnSubscribed]
+    subscription_id: int
 
 
 class BaseSession:
