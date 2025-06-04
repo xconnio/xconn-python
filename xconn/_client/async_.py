8e754c05b815a38e86999d627eb647bc18fd70d8
Omer Akram <om26er@genki.com>
2025-06-04 18:06:02 +0500

    implement auto reconnect in async App API

--- xconn/_client/async_.py
+++ xconn/_client/async_.py
@@ -1,3 +1,4 @@
+import asyncio
 import inspect
 
 from xconn import App
@@ -17,29 +18,56 @@ from xconn.async_session import AsyncSession
 from xconn.types import Event, Invocation, Result
 
 
-async def connect_async(app: App, config: ClientConfig, serve_schema: bool = False, start_router: bool = False):
+async def _setup(app: App, session: AsyncSession):
+    app.set_session(session)
+
+    for uri, func in app.procedures.items():
+        await register_async(session, uri, func)
+
+    for uri, func in app.topics.items():
+        await subscribe_async(session, uri, func)
+
+
+async def connect_async(app: App, config: ClientConfig, serve_schema=True, start_router: bool = False):
     if start_router:
         await start_server_async(config)
 
     auth = select_authenticator(config)
     client = AsyncClient(authenticator=auth)
 
-    session = await client.connect(config.url, config.realm)
-    print("connected", session.base_session.realm)
+    async def wait_and_connect(wait=10):
+        print(f"reconnecting in {wait} seconds...")
+        await asyncio.sleep(wait)
 
-    app.set_session(session)
+        try:
+            new_session = await client.connect(config.url, config.realm, on_connect, on_disconnect)
+        except Exception as e:
+            print(e)
 
-    docs = []
+            await wait_and_connect(wait)
+            return
 
-    for uri, func in app.procedures.items():
-        docs.append(collect_docs(uri, func, "procedure"))
-        await register_async(session, uri, func)
+        await _setup(app, new_session)
 
-    for uri, func in app.topics.items():
-        docs.append(collect_docs(uri, func, "topic"))
-        await subscribe_async(session, uri, func)
+    async def on_connect():
+        print("connected", config.realm)
+
+    async def on_disconnect():
+        print("disconnected", config.realm)
+        await wait_and_connect()
+
+    session = await client.connect(config.url, config.realm, on_connect, on_disconnect)
+    await _setup(app, session)
 
     if serve_schema:
+        docs = []
+
+        for uri, func in app.procedures.items():
+            docs.append(collect_docs(uri, func, "procedure"))
+
+        for uri, func in app.topics.items():
+            docs.append(collect_docs(uri, func, "topic"))
+
         await serve_schema_async(config.schema_host, config.schema_port, docs)
 
 
