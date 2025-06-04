f3502a683fc2b5c564b77b91a8b6a81382a5074b
Mahad <mahad@xconn.io>
2025-06-04 18:54:38 +0500

    add authentications in config and allow either file or flags for config

--- xconn/_client/types.py
+++ xconn/_client/types.py
@@ -1,14 +1,31 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class ClientConfig:
-    url: str
-    realm: str
-    authid: str
-    authmethod: str
+class ClientConfig(BaseModel):
     schema_host: str
     schema_port: int
+    url: str
+    realm: str
 
-    secret: str = ""
+    authid: str | None = None
+    authmethod: str | None = None
+    secret: str | None = None
+    ticket: str | None = None
+    private_key: str | None = None
+
+
+class CommandArgs(BaseModel):
+    app: str = Field(alias="APP")
+    url: str | None = None
+    realm: str | None = None
     directory: str | None = None
+    asyncio: bool
+    schema_host: str
+    schema_port: int
+    no_config: bool
+    start_router: bool = Field(alias="router")
+
+    authid: str | None = None
+    ticket: str | None = None
+    secret: str | None = None
+    private_key: str | None = None
