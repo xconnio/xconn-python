259c5f57922569f37fdf0c3b185ea01b97e0ddb4
Mahad <mahad@xconn.io>
2025-08-28 17:04:18 +0500

    add interoperability tests for Nexus

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 XCONN_URL = "ws://localhost:8080/ws"
 CROSSBAR_URL = "ws://localhost:8081/ws"
-ROUTER_URL = [XCONN_URL, CROSSBAR_URL]
+NEXUS_URL = "ws://localhost:8082/ws"
+ROUTER_URL = [XCONN_URL, CROSSBAR_URL, NEXUS_URL]
 REALM = "realm1"
