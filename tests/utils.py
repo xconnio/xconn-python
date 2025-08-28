53b0411232daadc259944db057e3a98d3de3a98b
Mahad <mahad@xconn.io>
2025-08-28 16:58:01 +0500

    add interoperability tests for XConn rawsocket

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
 XCONN_URL = "ws://localhost:8080/ws"
 CROSSBAR_URL = "ws://localhost:8081/ws"
 NEXUS_URL = "ws://localhost:8082/ws"
-ROUTER_URL = [XCONN_URL, CROSSBAR_URL, NEXUS_URL]
+XCONN_RAWSOCKET_URL = "unix:///tmp/nxt.sock"
+ROUTER_URL = [XCONN_URL, CROSSBAR_URL, NEXUS_URL, XCONN_RAWSOCKET_URL]
 REALM = "realm1"
