52e5ac1d346f5948482078b992d7bdeba6ccc9f7
Mahad <mahad@xconn.io>
2025-09-10 15:34:46 +0500

    update call signature in examples

--- examples/sync/rpc/caller.py
+++ examples/sync/rpc/caller.py
@@ -9,19 +9,19 @@ if __name__ == "__main__":
     caller = client.connect("ws://localhost:8080/ws", "realm1")
 
     # call procedure "io.xconn.echo"
-    result = caller.call(test_procedure_echo, "hello", "world", key="value")
+    result = caller.call(test_procedure_echo, ["hello", "world"], {"key": "value"})
     print(f"Result of procedure '{test_procedure_echo}': args={result.args}, kwargs={result.kwargs}")
 
     # call procedure "io.xconn.result" with args
-    caller.call(test_procedure_echo, "hello", "world")
+    caller.call(test_procedure_echo, ["hello", "world"])
 
     # call procedure "io.xconn.result" with kwargs
-    caller.call(test_procedure_echo, name="john")
+    caller.call(test_procedure_echo, kwargs={"name": "john"})
 
     # call procedure "io.xconn.result" with args & kwargs
-    caller.call(test_procedure_echo, 1, 2, name="john")
+    caller.call(test_procedure_echo, [1, 2], {"name": "john"})
 
-    sum_result = caller.call(test_procedure_sum, 2, 2, 6)
+    sum_result = caller.call(test_procedure_sum, [2, 2, 6])
     print(f"Sum={sum_result.args[0]}")
 
     # close connection to the server
